471a35b2fb544c99ef2d325fecf43cb3f7b762d5
Heena <hmotiyani15@gmail.com>
2020-05-29 00:21:15 +0530

    Added Basic HTML CSS and routing to the components

--- EventDashBoardApp/src/app/app-routing.module.ts
+++ EventDashBoardApp/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {BlogpostDetailComponent} from './blogpost/blogpost-detail/blogpost-detail.component';
+import {ContactFormComponent} from './cmspage/contact-form/contact-form.component';
+import {BlogpostListComponent} from './blogpost/blogpost-list/blogpost-list.component';
+const routes: Routes = [
+{path: '', redirectTo: '', pathMatch:'full'},
+{path: '**', component:PageNotFoundComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
